29cf54b007f78cad538f1bd11865a683b51c4700
GroupBuyAdmin <151867046+GroupBuyAdmin@users.noreply.github.com>
2023-12-07 22:06:59 +0800

    Update GbuyColor.java

--- src/groupbuyapp/Misc/ColorPalette/GbuyColor.java
+++ src/groupbuyapp/Misc/ColorPalette/GbuyColor.java
@@ -3,13 +3,13 @@ package groupbuyapp.Misc.ColorPalette;
 import java.awt.Color;
 
 public class GbuyColor {
-    public static final Color MAIN_COLOR = Color.decode("#F20530");
+    public static final Color MAIN_COLOR = Color.decode("#F20530");            //katong murag red
     
 
-    public static final Color PANEL_COLOR = Color.decode("#FFFFFF");
-    public static final Color PANEL_BACKGROUND_COLOR = Color.decode("#FDF8FF");
-    public static final Color MAIN_TEXT_COLOR = Color.decode("#000000");
-    public static final Color MAIN_TEXT_COLOR_ALT = Color.decode("#FFFFFF");
+    public static final Color PANEL_COLOR = Color.decode("#FFFFFF");            //white rani 
+    public static final Color PANEL_BACKGROUND_COLOR = Color.decode("#FDF8FF");    //background sa panel
+    public static final Color MAIN_TEXT_COLOR = Color.decode("#000000");        //black rani 
+    public static final Color MAIN_TEXT_COLOR_ALT = Color.decode("#FFFFFF");        //white rani 
 
 
 }
